9b62dc4e68ef8dc413be6284e076a9020bc51147
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-04-23 13:55:59 -0400

    testing psql on ec2

--- server/postgres.js
+++ server/postgres.js
@@ -1,8 +1,8 @@
 const { Client } = require('pg');
 
 exports.client = new Client({
-  host: 'ec2-54-159-123-212.compute-1.amazonaws.com',
-  user: '',
+  host: 'localhost',
+  user: 'postgres',
   password: '',
   database: 'fennel',
   port: 5432,
