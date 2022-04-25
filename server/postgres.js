296bc0681cc9c7de9746c98038bd5bf5b9fd4173
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-04-25 19:42:30 -0400

    test

--- server/postgres.js
+++ server/postgres.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg');
 
 exports.client = new Client({
-  host: 'ec2-54-226-80-51.compute-1.amazonaws.com',
+  host: '54.235.29.34',
   user: 'postgres',
   password: 'password',
   database: 'fennel',
