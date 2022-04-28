d40b02f9e03516f75587a51dd8390aa4b9d296c7
Joshua Son <joshuason@Joshuas-MacBook-Pro.local>
2022-04-28 12:01:29 -0400

    changed host

--- server/postgres.js
+++ server/postgres.js
@@ -1,8 +1,10 @@
 const { Client } = require('pg');
 require('dotenv').config();
 exports.client = new Client({
-  host: 'ec2-34-230-36-143.compute-1.amazonaws.com',
+  host: 'ec2-3-90-58-40.compute-1.amazonaws.com',
   user: process.env.user,
   password: process.env.password,
   database: 'fennel',
 });
+
+//artillery quick --count 20 --num 10 http://ec2-3-90-58-40.compute-1.amazonaws.com:3000/reviews/1
